3b9a8a18707624804a2f9fddb0420012d539daa3
vandoannguyen <vandoannguyenhaui@gmail.com>
2020-01-10 21:38:19 +0700

    init app

---
+++ lib/utils/CallNativeUtils.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+class CallNativeUtils {
+  static var _CHANNEL;
+  static MethodChannel _platform;
+  static void setChannel(channel) {
+    _CHANNEL = channel;
+    _platform = MethodChannel(_CHANNEL);
+  }
+
+  static Future invokeMethod(
+      {@required String method, dynamic aguments}) async {
+    return _platform.invokeMethod(method, aguments);
+  }
+
+  static void back(@required BuildContext context) {
+    Navigator.of(context).pop();
+  }
+}
